de9ab149039d84151ff621d1a065b4acc0f658b5
RJ-Lupin <156637181+RJ-Lupin@users.noreply.github.com>
2024-08-14 16:09:44 +0900

    Update plot_gene_expression.R

--- plot_gene_expression.R
+++ plot_gene_expression.R
@@ -19,7 +19,7 @@ plot_gene_expression <- function(seurat_obj, gene, order = FALSE, raster = FALSE
 # Seurat_v5 ---------------------------------------------------------------
 plot_gene_expression <- function(seurat_obj, gene, order = FALSE, raster = FALSE, pt_size = 0.001, legend.position = "none") {
   if (!gene %in% rownames(seurat_obj)) stop(paste("Gene", gene, "not found."))
-  max_value <- max(LayerData(seurat_obj.qc, assay = "RNA", layer = "data")[gene,]) # This line is different from Seurat v4
+  max_value <- max(LayerData(seurat_obj, assay = "RNA", layer = "data")[gene,]) # This line is different from Seurat v4
   FeaturePlot(seurat_obj, features = gene, order = order, raster = raster, reduction = "umap", pt.size = pt_size) +
     scale_colour_gradientn(colours = rev(c("#300000", "red", "#eeeeee")),
                            breaks = c(0, max_value),
